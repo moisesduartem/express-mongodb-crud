0e60e54a25e85d5a971d35a80fe152603e7dcc86
Moisés Mariano <moisesduartem@gmail.com>
2020-12-08 23:21:03 -0300

    import depedencies, connect to mongo & import routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const requireDir = require("require-dir");
+
+const app = express();
+
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true, 
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+requireDir("./src/models");
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
